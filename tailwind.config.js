1f75aa3f51cec1b05589c20cb45a8b75f7f0c8d4
dalowa <david.developer.work@gmail.com>
2023-06-18 23:40:27 -0500

    adding ColorA Blur version

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
       colors: {
         ColorA: "#637961",
+        ColorABlur: "#637961BF",
         ColorB: "#A0C49D",
         ColorC: "#C4D7B2",
         ColorD: "#E1ECC8",
