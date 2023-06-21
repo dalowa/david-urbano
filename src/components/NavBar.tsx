d758d6e88d7edb370cdcc5550889af36b2efdb67
dalowa <david.developer.work@gmail.com>
2023-06-20 20:10:53 -0500

    Now, it is, 100% responsive navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,8 +12,8 @@ export default function NavBar() {
       <div
         className={
           isOpen
-            ? `flex items-center justify-between px-3 py-1 fixed w-full z-20 top-0 bg-transparent xl:h-20 xl:w-auto  xl:bg-transparent sm:py-3`
-            : `flex items-center justify-between px-3 py-1 fixed w-full z-20 top-0 bg-ColorBOpacity50 xl:h-20 xl:w-auto  xl:bg-transparent sm:py-3`
+            ? `flex items-center justify-between px-3 py-1 fixed w-full z-20 top-0 bg-transparent lg:h-20 lg:w-auto  lg:bg-transparent sm:py-3`
+            : `flex items-center justify-between px-3 py-1 fixed w-full z-20 top-0 bg-ColorBOpacity50 lg:h-20 lg:w-auto  lg:bg-transparent sm:py-3`
         }
       >
         <Image
@@ -23,11 +23,11 @@ export default function NavBar() {
           alt="Logo"
           width={0o0}
           height={0o0}
-          className="h-auto w-8 sm:w-14 xl:h-16 xl:w-16"
+          className="h-auto w-8 sm:w-14 lg:h-16 lg:w-16"
         />
 
         <div
-          className="flex flex-col gap-1 xl:hidden items-center"
+          className="flex flex-col gap-1 lg:hidden items-center"
           onClick={() => {
             setIsOpen(!isOpen);
           }}
@@ -57,31 +57,31 @@ export default function NavBar() {
       </div>
       <div
         className={
-          isOpen ? `fixed flex w-screen h-screen z-10` : `hidden xl:flex`
+          isOpen ? `fixed flex w-screen h-screen z-10` : `hidden lg:flex`
         }
       >
         <div
-          className="w-2/5 bg-ColorE opacity-60 xl:hidden"
+          className="w-2/5 bg-ColorE opacity-60 lg:hidden"
           onClick={() => setIsOpen(!isOpen)}
         ></div>
-        <nav className="w-3/5 bg-ColorB shadow-xl shadow-black flex justify-center items-center xl:w-full xl:h-20 xl:fixed xl:bg-WhiteOpacity50 xl:shadow-md xl:shadow-ColorB xl:z-10">
-          <ul className="font-PrimaryFont text-ColorE flex flex-col gap-5 sm:text-2xl xl:items-center text-xl xl:flex-row xl:justify-end xl:w-full xl:text-ColorB xl:px-5 md:text-3xl xl:text-xl">
-            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl xl:py-1 xl:border-0 cursor-pointer hover:border-b-8 xl:border-ColorB md:p-5">
+        <nav className="w-3/5 bg-ColorB shadow-xl shadow-black flex justify-center items-center lg:w-full lg:h-20 lg:fixed lg:bg-WhiteOpacity50 lg:shadow-md lg:shadow-ColorB lg:z-10">
+          <ul className="font-PrimaryFont text-ColorE flex flex-col gap-5 sm:text-2xl lg:items-center text-xl lg:flex-row lg:justify-end lg:w-full lg:text-ColorB lg:px-5 md:text-3xl lg:text-xl lg:gap-3 xl:gap-5">
+            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl lg:py-1 lg:border-0 cursor-pointer hover:border-b-8 lg:border-ColorB md:p-5 lg:px-2">
               <a href="#Home">Home</a>
             </li>
-            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl xl:py-1 xl:border-0 cursor-pointer hover:border-b-8 xl:border-ColorB md:p-5">
+            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl lg:py-1 lg:border-0 cursor-pointer hover:border-b-8 lg:border-ColorB md:p-5 lg:px-2">
               <a href="#AboutMe">AboutMe</a>
             </li>
-            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl xl:py-1 xl:border-0 cursor-pointer hover:border-b-8 xl:border-ColorB md:p-5">
+            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl lg:py-1 lg:border-0 cursor-pointer hover:border-b-8 lg:border-ColorB md:p-5  lg:px-2">
               <a href="#Education">Education</a>
             </li>
-            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl xl:py-1 xl:border-0 cursor-pointer hover:border-b-8 xl:border-ColorB md:p-5">
+            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl lg:py-1 lg:border-0 cursor-pointer hover:border-b-8 lg:border-ColorB md:p-5  lg:px-2">
               <a href="#Projects">Projects</a>
             </li>
-            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl xl:py-1 xl:border-0 cursor-pointer hover:border-b-8 xl:border-ColorB md:p-5">
+            <li className="transition px-2 py-3 border-2 border-black flex justify-center rounded-xl lg:py-1 lg:border-0 cursor-pointer hover:border-b-8 lg:border-ColorB md:p-5  lg:px-2">
               <a href="#ContactMe">ContactMe</a>
             </li>
-            <li className=" px-2 py-3 border-ColorA border-2 flex justify-center text-ColorA bg-ColorE cursor-pointer transition hover:text-ColorE hover:bg-ColorA md:p-5 xl:py-2">
+            <li className=" px-2 py-3 border-ColorA border-2 flex justify-center text-ColorA bg-ColorE cursor-pointer transition hover:text-ColorE hover:bg-ColorA md:p-5 lg:py-2  lg:px-2">
               <a>Resume</a>
             </li>
           </ul>
