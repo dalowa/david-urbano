8a39ca668f8c16136a0c63b412d6a306bb278c73
dalowa <david.developer.work@gmail.com>
2023-06-22 01:36:47 -0500

    better responsive

--- src/components/TechIconCard.tsx
+++ src/components/TechIconCard.tsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 export default function TechIconCard({ name }: { name: string }) {
   return (
-    <div className="transition flex flex-col w-24 justify-between h-32 items-center xl:hover:scale-125">
+    <div className="transition flex flex-col w-3/12 justify-between h-32 items-center hover:scale-125 md:h-40 lg:w-3/12 lg:h-28 2xl:h-32">
       <Image
         src={`https://raw.githubusercontent.com/dalowa/david-urbano/main/public/techIcons/${name}.png`}
         alt={`ICON ${name.toLocaleUpperCase()}`}
         width={80}
         height={0o0}
         priority={true}
-        className="w-20 h-auto xl:w-28"
+        className="w-full h-auto md:w-9/12 xl:w-10/12 2xl:w-7/12"
       />
-      <h5 className="text-center xl:text-xl">{name.toLocaleUpperCase()}</h5>
+      <h5 className="text-center md:text-xl lg:text-sm 2xl:text-base">
+        {name.toLocaleUpperCase()}
+      </h5>
     </div>
   );
 }
