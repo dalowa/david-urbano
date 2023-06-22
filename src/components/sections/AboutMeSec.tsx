bf35920c8e064a30324e91cd51dd2c05d4315c4e
dalowa <david.developer.work@gmail.com>
2023-06-22 01:36:10 -0500

    responsive upgrade

--- src/components/sections/AboutMeSec.tsx
+++ src/components/sections/AboutMeSec.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function AboutMeSec() {
   return (
     <section
-      className="bg-ColorC p-5 sm:p-10 xl:pt-24 xl:pb-36 xl:px-36 xl:w-full xl:flex xl:justify-center"
+      className="bg-ColorC sm:p-10 xl:pt-24 xl:pb-36  xl:w-full xl:flex xl:justify-center"
       id="AboutMe"
     >
-      <div className="flex flex-col gap-5 xl:flex-col xl:px-0 xl:gap-1 xl:w-full xl:max-w-6xl xl:justify-center xl:items-center">
+      <div className="flex flex-col gap-5 xl:flex-col xl:px-0 xl:gap-1 xl:w-10/12 xl:justify-center xl:items-center 2xl:w-11/12">
         <h2 className="font-PrimaryFont text-ColorE text-2xl md:text-3xl lg:text-4xl xl:text-5xl xl:w-full xl:my-5">{`=> About Me`}</h2>
-        <div className="flex flex-col gap-5 xl:flex-row xl:p-3">
-          <div className="flex flex-col gap-5 xl:gap-10">
-            <p className="font-SecondaryFont text-base lg:text-2xl lg:w-4/5 mx-auto md:text-xl md:text-right xl:text-left">
+        <div className="flex flex-col gap-5 lg:flex-row xl:p-3 2xl:p-0">
+          <div className="flex flex-col gap-5 xl:gap-10 lg:w-6/12 2xl:justify-end 2xl:items-end">
+            <p className="font-SecondaryFont text-base lg:text-xl lg:w-full mx-auto md:text-xl md:text-right xl:text-left xl:text-2xl 2xl:text-3xl 2xl:w-10/12">
               {`I am a young bilingual web developer located in Lima, Peru. I love the
       idea of being able to create something that can change someone's life
       for the better and I like to think that we can make a better world
@@ -23,7 +23,7 @@ export default function AboutMeSec() {
               </a>
             </h4>
           </div>
-          <div className="flex w-full justify-center items-center h-72">
+          <div className="flex w-full justify-center items-center h-72 lg:w-6/12">
             <div className="border-2 border-black p-1 bg-ColorB transition-all ease-in-out duration-100 hover:border-none hover:p-0 rounded-lg">
               <Image
                 src={
@@ -32,7 +32,7 @@ export default function AboutMeSec() {
                 alt="Profile David Urbano"
                 width={0o0}
                 height={0o0}
-                className="w-64 h-auto border-2 border-black bg-ColorE hover:border-0 transition-all ease-in-out duration-100 hover:shadow-black	hover:shadow-lg	rounded-lg hover:bg-white xl:w-96"
+                className="w-full h-auto border-2 border-black bg-ColorE hover:border-0 transition-all ease-in-out duration-100 hover:shadow-black	hover:shadow-lg	rounded-lg hover:bg-white xl:w-96"
               />
             </div>
           </div>
