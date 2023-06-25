bccb7864f1055cb194e76bf2fe547f15600dfd9e
dalowa <david.developer.work@gmail.com>
2023-06-24 23:59:34 -0500

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import NavBar from "@/components/NavBar";
-import IconDeploy from "@/components/icons/IconDeploy";
-import AboutMeSec from "@/components/sections/AboutMeSec";
-import EducationSec from "@/components/sections/EducationSec";
+
 import HomeSec from "@/components/sections/HomeSec";
-import ProjectsSec from "@/components/sections/ProjectsSec";
 
 export default function Home() {
   return (
