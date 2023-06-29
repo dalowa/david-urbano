472386183728f216102bcfe220dc83066da69d58
dalowa <david.developer.work@gmail.com>
2023-06-28 22:14:34 -0500

    fontsize subtitle

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       fontSize: {
         Title: "clamp(35px, 7vw, 80px)",
-        SubTitle: "clamp(10px, 5vw, 17px)",
+        SubTitle: "clamp(10px, 5vw, 19px)",
         navBarMobile: "clamp(10px, 5vw, 20px)",
         navBarDesktop: "clamp(10px, 5vw, 15px)",
         titleSection: "clamp(22px, 5vw, 32px)",
