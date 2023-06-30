7533fa8f611521fd1fc534f4b81a75e5630ef617
dalowa <david.developer.work@gmail.com>
2023-06-29 23:45:37 -0500

    .

--- src/components/sections/HomeSec.tsx
+++ src/components/sections/HomeSec.tsx
@@ -18,7 +18,7 @@ export default function HomeSec() {
       <p className="text-ColorGris mt-3 text-SubTitle md:w-11/12 lg:w-9/12 2xl:w-7/12">
         I am a bilingual web developer, I love all the possibilities that
         software development offers. Currently, developing independent projects
-        and looking for new challenges to expand my knowledge
+        and looking for new challenges to expand my knowledge.
       </p>
       <a className="text-ColorCeleste py-3 px-6 border border-ColorCeleste border-solid rounded-md mt-10 cursor-pointer transition-colors hover:bg-ColorCeleste hover:text-ColorAzulMarino hover:shadow-md hover:shadow-ColorCeleste">
         Check out my work
