94e143cacfbc178e59b4a3b73f25acef3989348f
dalowa <david.developer.work@gmail.com>
2023-07-02 23:05:33 -0500

    without tags in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/dalowa/david-urbano/main/public/profile.png"
-        />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
         <link
           rel="icon"
           href="https://raw.githubusercontent.com/dalowa/david-urbano/cb99741e256698f6151e2ba880a8aa50723d6eeb/public/new-d.svg"
