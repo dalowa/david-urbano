6a4674bf7f7edeeead694960f994c488cc2527e7
dalowa <david.developer.work@gmail.com>
2023-07-04 02:29:51 -0500

    two links more working

--- src/components/sections/FooterSec.tsx
+++ src/components/sections/FooterSec.tsx
@@ -13,8 +13,14 @@ export default function FooterSec() {
         <div className=" flex w-full gap-10 lg:gap-6 justify-center items-center py-4 lg:fixed lg:left-10 lg:bottom-0 lg:flex-col lg:w-auto lg:p-0">
           <IconGithub Link="https://github.com/dalowa" Class={iconClass} />
           <IconInstagram Link="" Class={iconClass} />
-          <IconTwitter Link="" Class={iconClass} />
-          <IconLinkedin Link="" Class={iconClass} />
+          <IconTwitter
+            Link="https://twitter.com/David_urbano_"
+            Class={iconClass}
+          />
+          <IconLinkedin
+            Link="https://www.linkedin.com/in/david-urbano-82a151223/"
+            Class={iconClass}
+          />
           <div className="w-px h-32 bg-ColorGrisClaro hidden lg:inline"></div>
         </div>
         <div className="hidden lg:flex flex-col items-center gap-5 lg:fixed lg:right-10 lg:bottom-0">
