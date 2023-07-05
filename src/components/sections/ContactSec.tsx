c94c849bf16015a0f4795883502c736e5e6c2791
dalowa <david.developer.work@gmail.com>
2023-07-05 16:20:31 -0500

    improving padding siz

--- src/components/sections/ContactSec.tsx
+++ src/components/sections/ContactSec.tsx
@@ -7,7 +7,7 @@ export default function ContactSec() {
         id="Contact"
         className="bg-ColorAzulMarino py-28 min-h-screen flex flex-col items-center justify-center w-10/12 mx-auto gap-12 sm:gap-10 lg:gap-3 lg:w-10/12 xl:w-9/12 2xl:w-7/12 "
       >
-        <div className="text-ColorGrisClaro justify-center text-titleSection font-fontInter font-semibold flex w-full gap-2 items-center leading-none mb-20">
+        <div className="text-ColorGrisClaro justify-center text-titleSection font-fontInter font-semibold flex w-full gap-2 items-center leading-none mb-16">
           <span className="text-ColorCeleste text-numberTitleSection">03.</span>
           <h2 className="">Contact Me</h2>
           {/* <div className="h-0.5 w-6/12 bg-ColorGrisOscuro"></div> */}
