bc89dad00040a349377e582fab7a5c809aecfca5
dalowa <david.developer.work@gmail.com>
2023-07-05 16:19:28 -0500

    Fixing padding

--- src/components/sections/ContactSec.tsx
+++ src/components/sections/ContactSec.tsx
@@ -5,9 +5,9 @@ export default function ContactSec() {
     <>
       <section
         id="Contact"
-        className="bg-ColorAzulMarino py-32 min-h-screen flex flex-col items-center justify-center w-10/12 mx-auto gap-16 sm:gap-10 lg:gap-3 lg:w-10/12 xl:w-9/12 2xl:w-7/12 "
+        className="bg-ColorAzulMarino py-28 min-h-screen flex flex-col items-center justify-center w-10/12 mx-auto gap-12 sm:gap-10 lg:gap-3 lg:w-10/12 xl:w-9/12 2xl:w-7/12 "
       >
-        <div className="text-ColorGrisClaro justify-center text-titleSection font-fontInter font-semibold flex w-full gap-2 items-center leading-none">
+        <div className="text-ColorGrisClaro justify-center text-titleSection font-fontInter font-semibold flex w-full gap-2 items-center leading-none mb-20">
           <span className="text-ColorCeleste text-numberTitleSection">03.</span>
           <h2 className="">Contact Me</h2>
           {/* <div className="h-0.5 w-6/12 bg-ColorGrisOscuro"></div> */}
