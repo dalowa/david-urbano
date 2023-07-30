f222d8e29a299010c5115f19857c3b0f450fc529
dalowa <david.developer.work@gmail.com>
2023-07-30 14:43:31 -0500

    List of projects, ready to modify and add new projects

---
+++ src/data/ProjectsDataList.ts
@@ -0,0 +1,30 @@
+const ProjectsDataList = [{
+    isRight:true,
+    id:"01",
+    type:"Personal Project",
+    title:"The Main Page",
+    description:"A website where you can search anything with any search engine and a list of tasks ordered by date priority. In the coming months the characteristics of this page will be expanded",
+    techList:["NextJS", "TailwindCSS", "Typescript", "Zustand"],
+    linkDeploy:"https://the-definitive-main-page.vercel.app/",
+    linkGithub:"https://github.com/dalowa/the-definitive-main-page"
+},{
+    isRight:false,
+    id:"01",
+    type:"Personal Project",
+    title:"The Main Page",
+    description:"A website where you can search anything with any search engine and a list of tasks ordered by date priority. In the coming months the characteristics of this page will be expanded",
+    techList:["NextJS", "TailwindCSS", "Typescript", "Zustand"],
+    linkDeploy:"https://the-definitive-main-page.vercel.app/",
+    linkGithub:"https://github.com/dalowa/the-definitive-main-page"
+},{
+    isRight:true,
+    id:"01",
+    type:"Personal Project",
+    title:"The Main Page",
+    description:"A website where you can search anything with any search engine and a list of tasks ordered by date priority. In the coming months the characteristics of this page will be expanded",
+    techList:["NextJS", "TailwindCSS", "Typescript", "Zustand"],
+    linkDeploy:"https://the-definitive-main-page.vercel.app/",
+    linkGithub:"https://github.com/dalowa/the-definitive-main-page"
+}]
+
+export default ProjectsDataList;
